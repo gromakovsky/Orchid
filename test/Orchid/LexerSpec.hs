a5bd68e27f432809caaa520383ecdf14773b24ed
Ivan Gromakovsky <gie1994@gmail.com>
2016-04-17 18:25:38 +0300

    Add dummy test-suite

---
+++ test/Orchid/LexerSpec.hs
@@ -0,0 +1,10 @@
+-- | Lexer specification.
+
+module Orchid.LexerSpec
+       ( spec
+       ) where
+
+import           Test.Hspec (Spec, describe)
+
+spec :: Spec
+spec = describe "Lexer" $ do return ()
